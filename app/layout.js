2272bc6174a30b884274f9858e79202a334afc9e
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2024-12-22 14:40:49 +0530

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -27,16 +27,14 @@ export default function RootLayout({ children }) {
 
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
-      <Link href={"https://youtu.be/lkjrUW8fI40"} target="_blank" className="">
-        <p
-          className={
-            "text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  " +
-            fugaz.className
-          }
-        >
-          Built with 💛
-        </p>
-      </Link>
+      <p
+        className={
+          "text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  " +
+          fugaz.className
+        }
+      >
+        Built with 💛
+      </p>
     </footer>
   );
 
